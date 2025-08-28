f4d722549cba6393ec41bcdc0f5f27564c494365
James Hughes <jhughes2712@gmail.com>
2025-08-28 14:53:59 +0100

    Add tenant ID.

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ import traceback
 # -----------------------------
 APP_ID = os.environ.get("MicrosoftAppId", "")
 APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+APP_TENANT_ID = os.environ.get("MicrosoftAppTenantId", "")
 print(f"DEBUG: MicrosoftAppId={APP_ID}")
 print(f"DEBUG: MicrosoftAppPassword={'SET' if APP_PASSWORD else 'NOT SET'}")
 
